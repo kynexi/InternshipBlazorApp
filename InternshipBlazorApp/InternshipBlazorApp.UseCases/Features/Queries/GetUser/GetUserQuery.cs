d6c1291b7e63a16c76ebc44e24dc868cae94635d
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 20:48:51 +0300

    fix(UseCases): add Id param to GetUsersQuery

--- InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Queries/GetUser/GetUserQuery.cs
+++ InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Queries/GetUser/GetUserQuery.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using InternshipBlazorApp.Core.Entities;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InternshipBlazorApp.UseCases.Features.Queries.GetUser
-{
-    internal class GetUserQuery
-    {
-    }
-}
+namespace InternshipBlazorApp.UseCases.Features.Queries.GetUser;
+
+public record GetUserQuery(Guid Id): IRequest<User?>;
