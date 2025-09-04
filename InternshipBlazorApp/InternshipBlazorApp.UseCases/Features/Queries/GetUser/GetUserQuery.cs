3ef6eeb0da62b48df077fa7476bd7d78a93da230
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 20:44:32 +0300

    feat(UseCases): add GetUser mediator query

---
+++ InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipBlazorApp.UseCases.Features.Queries.GetUser
+{
+    internal class GetUserQuery
+    {
+    }
+}
---
+++ InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Queries/GetUser/GetUserQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using InternshipBlazorApp.Core.Entities;
+using InternshipBlazorApp.Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipBlazorApp.UseCases.Features.Queries.GetUser;
+
+internal class GetUserQueryHandler: IRequestHandler<GetUserQuery, User?>
+{
+    private readonly IUsersRepository _repository;
+
+    public GetUserQueryHandler(IUsersRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<User?> Handle(GetUserQuery request, CancellationToken cancellationToken)
+    {
+        return await _repository.GetUserAsync(request.Id, cancellationToken);
+    }
+}
