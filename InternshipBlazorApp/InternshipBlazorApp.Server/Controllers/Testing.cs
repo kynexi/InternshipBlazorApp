facc9e6db5a256756a0cbbdcb0be2feffcd9d2d8
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 16:06:58 +0300

    feat(Server): add a test route midpoint

---
+++ InternshipBlazorApp/InternshipBlazorApp.Server/Controllers/Testing.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InternshipBlazorApp.Server.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class Testing : ControllerBase
+    {
+        private readonly ILogger<Testing> _logger;
+
+        public Testing(ILogger<Testing> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("testing")]
+        public IActionResult GetTesting()
+        {
+            return Ok("This is a test!");
+        }
+    }
+}
