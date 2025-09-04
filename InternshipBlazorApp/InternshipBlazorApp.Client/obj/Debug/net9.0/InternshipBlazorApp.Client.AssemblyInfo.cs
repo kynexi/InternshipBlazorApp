d8f0ab83890b3f4f9e07649a289abd67fad59008
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 15:20:15 +0300

    chore(Server): add docker containers for server project

--- InternshipBlazorApp/InternshipBlazorApp.Client/obj/Debug/net9.0/InternshipBlazorApp.Client.AssemblyInfo.cs
+++ InternshipBlazorApp/InternshipBlazorApp.Client/obj/Debug/net9.0/InternshipBlazorApp.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InternshipBlazorApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e00b239eac485c05fbad208435536b739420a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("InternshipBlazorApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InternshipBlazorApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
