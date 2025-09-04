eac843b62882736d427ebb056f15b558731ede72
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 18:48:07 +0300

    feat(Infrastructure): add the initial migration

---
+++ InternshipBlazorApp/InternshipBlazorApp.Infrastructure/Services/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternshipBlazorApp.Core.Entities;
+
+namespace InternshipBlazorApp.Infrastructure.Services
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
