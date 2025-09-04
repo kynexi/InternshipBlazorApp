7eb86774332a4f1036eda4da18d04707afff8e91
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 16:30:28 +0300

    feat(Core): add User entity

---
+++ InternshipBlazorApp/InternshipBlazorApp.Core/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipBlazorApp.Core.Entities;
+
+internal class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public bool IsMarried { get; set; }
+}
