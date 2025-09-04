f669848b8daa97f2fbf3a7b448ed42cef3b798d2
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 16:52:15 +0300

    refactor(Core): change User entity visibility from internal to public

--- InternshipBlazorApp/InternshipBlazorApp.Core/Entities/User.cs
+++ InternshipBlazorApp/InternshipBlazorApp.Core/Entities/User.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace InternshipBlazorApp.Core.Entities;
 
-internal class User
+public class User
 {
     public Guid Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
