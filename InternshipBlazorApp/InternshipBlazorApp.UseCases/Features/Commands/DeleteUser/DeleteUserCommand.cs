b1452067e2212e2a453ea5c5203b18b585c8c157
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 20:47:43 +0300

    feat(UseCases): add DeleteUser mediator command

---
+++ InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using InternshipBlazorApp.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipBlazorApp.UseCases.Features.Commands.DeleteUser;
+public record DeleteUserCommand(Guid Id): IRequest<User?>;
---
+++ InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using InternshipBlazorApp.Core.Entities;
+using InternshipBlazorApp.Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipBlazorApp.UseCases.Features.Commands.DeleteUser;
+public class DeleteUserCommandHandler: IRequestHandler<DeleteUserCommand, User?>
+{
+    private readonly IUsersRepository _repository;
+
+    public DeleteUserCommandHandler(IUsersRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<User?> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        return await _repository.DeleteUserAsync(request.Id, cancellationToken);
+    }
+}
