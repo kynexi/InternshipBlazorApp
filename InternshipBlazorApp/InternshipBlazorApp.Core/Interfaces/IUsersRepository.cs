6ca44b7b1add14d8ee612f7010b44a54ccab0abf
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 20:24:00 +0300

    feat(Core): populate IUsersRepository

--- InternshipBlazorApp/InternshipBlazorApp.Core/Interfaces/IUsersRepository.cs
+++ InternshipBlazorApp/InternshipBlazorApp.Core/Interfaces/IUsersRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using InternshipBlazorApp.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InternshipBlazorApp.Core.Interfaces
+namespace InternshipBlazorApp.Core.Interfaces;
+
+public interface IUsersRepository
 {
-    internal class IUsersRepository
-    {
-    }
+    public Task<User> CreateUserAsync(User user, CancellationToken cancellationToken);
+    public Task<User?> GetUserAsync(Guid id, CancellationToken cancellationToken);
+    public Task<User?> DeleteUserAsync(Guid id, CancellationToken cancellationToken);
 }
+ 
