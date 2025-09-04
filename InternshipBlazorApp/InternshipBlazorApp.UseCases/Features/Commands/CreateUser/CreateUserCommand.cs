3427ccd5966a4128d95ee29e6c623d9a6b07b3ea
kynexi <184446386+kynexi@users.noreply.github.com>
2025-09-04 20:35:01 +0300

    feat(UseCases): add CreateUser mediator command

---
+++ InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using InternshipBlazorApp.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipBlazorApp.UseCases.Features.Commands.CreateUser;
+
+public record CreateUserCommand(string FirstName, string LastName, int Age, bool IsMarried): IRequest<User>;
---
+++ InternshipBlazorApp/InternshipBlazorApp.UseCases/Features/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using InternshipBlazorApp.Core.Entities;
+using InternshipBlazorApp.Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipBlazorApp.UseCases.Features.Commands.CreateUser;
+
+internal class CreateUserCommandHandler: IRequestHandler<CreateUserCommand, User>
+{
+    private readonly IUsersRepository _repository;
+
+    public CreateUserCommandHandler(IUsersRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<User> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = new User
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Age = request.Age,
+            IsMarried = request.IsMarried
+        };
+
+        return await _repository.CreateUserAsync(user, cancellationToken);
+    }
+}
